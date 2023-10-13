f151279f66e3fe598de3f5c21de94e010d53ee26
DhiruNit <dhirendrasingh10dec@gmail.com>
2023-10-13 08:45:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,54 +4,54 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "myepamtest" {
   name     = "myResourceGroup"
   location = "East US"
 }
 
 # Create a public IP address
-resource "azurerm_public_ip" "example" {
+resource "azurerm_public_ip" "myepamtest" {
   name                = "myPublicIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.myepamtest.location
+  resource_group_name = azurerm_resource_group.myepamtest.name
   allocation_method   = "Dynamic"
 }
 
 # Create a virtual network
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "myepamtest" {
   name                = "myVNet"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.myepamtest.location
+  resource_group_name = azurerm_resource_group.myepamtest.name
   address_space       = ["10.0.0.0/16"]
 }
 
 # Create a subnet
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "myepamtest" {
   name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.myepamtest.name
+  virtual_network_name = azurerm_virtual_network.myepamtest.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create a network interface
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "myepamtest" {
   name                = "myNIC"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.myepamtest.location
+  resource_group_name = azurerm_resource_group.myepamtest.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.myepamtest.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Create a virtual machine
-resource "azurerm_virtual_machine" "example" {
+resource "azurerm_virtual_machine" "myepamtest" {
   name                  = "myVM"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
+  location              = azurerm_resource_group.myepamtest.location
+  resource_group_name   = azurerm_resource_group.myepamtest.name
+  network_interface_ids = [azurerm_network_interface.myepamtest.id]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
